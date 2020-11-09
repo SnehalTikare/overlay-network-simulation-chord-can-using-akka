5234c3eaa0d0ad1142e69b6b4a453e50ae001450
Sakina Master <sakina.master2050@gmail.com>
2020-11-09 17:21:41 -0600

    Basic akka implementation

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -1,6 +1,59 @@
-object Driver {
-    def main(args: Array[String]) {
-      print("Hello World")
-      print("test")
+//object Driver {
+//    def main(args: Array[String]) {
+//      print("Hello World")
+//      print("test")
+//    }
+//}
+
+import akka.actor._
+import java.security.MessageDigest
+
+class NodeSupervisor(system: ActorSystem, numNodes: Int) extends Actor{
+  override def receive: Receive = {
+    case createChordNodes => {
+      //ActorRef - Reference that points to the actor instance
+      val actorNodes = new Array[ActorRef](numNodes)
+      for (x <- 0 until numNodes){
+        actorNodes(x) = system.actorOf(Props(new ChordNode(x)), name = "Node" + x + "-in-chord-ring")
+        actorNodes(x) ! "createHash"
+      }
+      println("Chord nodes created.")
     }
+  }
+}
+
+
+class ChordNode(id: Int) extends Actor {
+  val nodeId: Int = id
+
+  //Generates an m-bit identifier for node using md5 hash function.
+  def md5(s: String): String = {
+    MessageDigest.getInstance("MD5").digest(s.getBytes).toString
+  }
+
+  override def receive: Receive = {
+    case createHash => {
+      val hashVal: String = md5(nodeId.toString)
+      println("Node Id: " + nodeId + " HashVal = " + hashVal)
+    }
+    case _ => {
+      print("Default")
+    }
+  }
+}
+
+
+object Driver {
+  def main(args: Array[String]): Unit = {
+    val numNodes = 3
+    //val numUsers = 3
+    //val numRequests = 2
+
+    //An ActorSystem is the initial entry point into Akka.
+    val actorSystem: ActorSystem = ActorSystem("Actor-System")
+
+    val nodeSupervisor = actorSystem.actorOf(Props(new NodeSupervisor(actorSystem, numNodes)), name = "NodeActors")
+    nodeSupervisor ! "createChordNodes"
+
+  }
 }
