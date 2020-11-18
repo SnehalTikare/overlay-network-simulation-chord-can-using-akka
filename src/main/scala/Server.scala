24522a5e0650e9301cd01a73537b6532648dc9c1
snehaltikare <snehaltkr@gmail.com>
2020-11-18 17:05:22 -0600

    Updated Read me

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -17,7 +17,7 @@ class Server extends LazyLogging{
   def start(serverActorSystem: ActorSystem, chordNodes : List[Int]): Unit = {
 
     val requestPath = path("ons") {
-      withRequestTimeout(40000.seconds)
+      withRequestTimeout(50000.seconds)
       concat(
         get {
           parameter("key".as[String]) { key =>
